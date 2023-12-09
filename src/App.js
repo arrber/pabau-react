20e6e3280082aa47e8277a8204161b5539ba15da
arber <arberbreznica98@gmail.com>
2023-12-09 10:49:48 +0100

    ArberBreznica/14/09/2003/--cleaning-code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Selection from './Pages/MainSelection/Selection';
 import Details from './Pages/Details/Details';
--- src/Components/OptionCards/OptionsCards.js
+++ src/Components/OptionCards/OptionsCards.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
 const OptionsCards = ({selectedOption}) => {
-    // const handleClick = () => {
-    //     selectedOption = 1;
-    // }
 
     return (
         <div className="list-holder">
@@ -15,10 +12,6 @@ const OptionsCards = ({selectedOption}) => {
                 <li><NavLink onClick={selectedOption} to="/detail" className="link">Filan Fisteku2</NavLink></li>
             </ul>
         </div>
-        // <div onClick={onSelectOption} className="card">
-        //     <img src={props.image} alt="" />
-        //     <p>{props.name}</p>
-        // </div>
     );
 };
 
